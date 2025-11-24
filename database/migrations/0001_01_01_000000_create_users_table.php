496bf6a69b1efe2d91c52c762a75aafc98c1da52
raffay <abdulraffay885@gmail.com>
2025-11-24 14:43:07 +0500

    Update password field in users table migration to make it required

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->enum('user_type', ['admin', 'customer'])->default('customer');
             $table->rememberToken();
             $table->timestamps();
